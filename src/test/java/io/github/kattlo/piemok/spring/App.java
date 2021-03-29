d84fa51ec16efd9a42b19a35f9b4001b4f169efa
Fábio José <fabiojose@gmail.com>
2021-03-29 11:50:04 -0300

    Tests with mocked producer and consumer
    Signed-off-by: Fábio José <fabiojose@gmail.com>

--- src/test/java/io/github/kattlo/piemok/spring/App.java
+++ src/test/java/io/github/kattlo/piemok/spring/App.java
@@ -1,13 +1,42 @@
 package io.github.kattlo.piemok.spring;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
+
 
 @SpringBootApplication
 public class App {
+
+    @Autowired
+    KafkaTemplate<String, Object> producer;
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        producer.partitionsFor("topic");
+    }
+
+    @KafkaListener(
+        id = "my-group.id",
+        idIsGroup = true,
+        topics = "my-topic-name"
+    )
+    public void listener(@Payload ConsumerRecord<String, Object> r) throws Exception {
+        // ...
+
+        System.out.println("consumed record " + r);
+
+        // ...
+
+    }
 }
--- src/test/java/io/github/kattlo/piemok/spring/MockedKafkaConfigTest.java
+++ src/test/java/io/github/kattlo/piemok/spring/MockedKafkaConfigTest.java
@@ -1,5 +1,6 @@
 package io.github.kattlo.piemok.spring;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,12 @@ public class MockedKafkaConfigTest {
     @Autowired
     DefaultKafkaProducerFactory<String, Object> producerFactory;
 
+    @Autowired
+    MockedKafkaConsumerFactory<String, Object> consumerMockedFactory;
+
+    @Autowired
+    MockedKafkaProducerFactory<String, Object> producerMockedFactory;
+
     @Test
     public void should_load_mocked_consumer_as_default_factory() {
 
@@ -36,4 +43,28 @@ public class MockedKafkaConfigTest {
         assertTrue(producerFactory instanceof MockedKafkaProducerFactory);
     }
 
+    @Test
+    public void should_return_the_mocked_consumer() {
+
+        var mocked = consumerMockedFactory.of("my-group.id");
+        assertNotNull(mocked);
+        assertTrue(mocked.isPresent());
+    }
+
+    @Test
+    public void should_return_the_mocked_producer() {
+
+        assertTrue(producerMockedFactory.producer().isPresent());
+    }
+
+    @Test
+    public void should_reset_the_mocked_consumer() {
+
+        var mocked = consumerMockedFactory.of("my-group.id");
+        mocked.ifPresent(m -> {
+
+            m.reset("my-topic", null, "some-value");
+
+        });
+    }
 }
